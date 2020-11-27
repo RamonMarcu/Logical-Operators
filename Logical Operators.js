0239ed9bcd48449375b027be9c7245a34facd77a
Ramon Marcu <70721540+RamonMarcu@users.noreply.github.com>
2020-11-27 18:32:10 +0200

    Add files via upload

---
+++ Logical Operators.js
@@ -0,0 +1,35 @@
+let mood = 'sleepy';
+let tirednessLevel = 9;
+if ( mood === 'sleepy' && tirednessLevel > 8) 
+  {
+    console.log('time to sleep')
+  } else {
+    console.log('not bed time yet')
+  }
+
+
+/* if run the first example, console displays: time to sleep */
+
+
+let mood = 'sleepy';
+let tirednessLevel = 7;
+if ( mood === 'sleepy' && tirednessLevel > 8) 
+  {
+    console.log('time to sleep')
+  } else {
+    console.log('not bed time yet')
+  }
+
+/* if run the second example, console displays: not bed time yet */
+
+let mood = 'sleepy';
+let tirednessLevel = 7;
+if ( mood === 'sleepy' || tirednessLevel > 4+5) 
+  {
+    console.log('time to sleep')
+  } else {
+    console.log('not bed time yet')
+  }
+
+/* if run the third example, console displays: time to sleep */
+
